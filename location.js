f4988e3481963f9e8b9fd995c77e532780bbc81e
Wichanart <azealia@gmail.com>
2017-07-31 19:36:32 +0700

    Create location.js

---
+++ location.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
+
+//nom install body-parser --save
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//We can test this using POSTman and send information as application/x-www-form-urlencoded
+app.post( '/', function( req, res) {
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    console.log(latitude + ',' + longitude);
+    res.send(latitude + ',' + longitude);
+})
+
+app.listen(port);
+console.log('Server started! At http://locahost:' + port);
